a4d663ce344c78e89fae77d61e0ea289925a684a
Rishama <gracerishama@gmail.com>
2026-04-10 16:54:07 +0100

    intro-javascript

---
+++ intro.js
@@ -0,0 +1,8 @@
+// defining my information
+const name  = Iliya Grace Rishama ;
+const age = 20 ;
+const favoriteLanguage = Javascript ;
+
+console.log("Name: " + name) ;
+console.log("Age: " + age) ;
+console.log("FavoriteLanguage: " + favoriteLanguage) ;
